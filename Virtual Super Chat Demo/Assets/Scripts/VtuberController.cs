a69be74548b2fcd3c964072c605f0bebe946e880
Kyohei Mizutani <kyohei.mizutani@gmail.com>
2018-06-11 12:40:28 +0900

    Add basic vtuber functions with Final IK

---
+++ Virtual Super Chat Demo/Assets/Scripts/VtuberController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VtuberController : MonoBehaviour {
+
+
+    private void Start()
+    {
+        UnityEngine.XR.XRSettings.showDeviceView = false;
+    }
+}
